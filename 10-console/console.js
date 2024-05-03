52fe49e7ae0d7aaf98f6827bafeb8c6a48f46003
toules2097868 <etsoumar01@monccnb.ca>
2024-05-03 16:38:59 -0300

    10-console

---
+++ 10-console/console.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const temperature = 21.3
+const fruits = ['pomme', 'poire', 'orange']
+
+// Afficher les valeurs
+console.log("La température est de " + temperature + " degrés.");
+console.log("Les fruits sont : " + fruits);
+
+// Afficher les types
+console.log("Le type de la variable 'temperature' est " + typeof temperature + ".");
+console.log("Le type de la variable 'fruits' est " + typeof fruits + ".");
