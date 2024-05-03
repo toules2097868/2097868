7e6e4167e7a3f4be36dccf29f09787a6448cbef8
toules2097868 <etsoumar01@monccnb.ca>
2024-05-03 16:19:40 -0300

    Ajout du dossier 20-chaine-caractere

---
+++ 20-chaine-caractere/chaine-caractere.js
@@ -0,0 +1,24 @@
+/**
+ * Exercice - chaine-caractere
+ *
+ * Utiliser les fonctions JavaScript pour le traitement des chaines de caractères.
+ *
+ * Consignes:
+ *  Créer le fichier js/chaine-caractere.js à partir du code suivant.
+ *  Compléter le code manquant pour obtenir le résultat de la maquette.
+ *
+ * Note: Seulement l'objet console est nécessaire, pas d'itération ou autre structure de contrôle
+ */
+'use strict'
+
+const SENTENCE = 'The strength of JavaScript is that you can do anything. The weakness is that you will.'
+console.log('La phrase entière ')
+console.log('La longueur de la chaine de caractères : ')
+console.log('La position du mot JavaScritp : ')
+console.log('La position du dernier is : ')
+console.log('La dernière phrase seulement: ')
+console.log('Les 10 carateres a partir du 17iem caractere: ')
+console.log('Combien de mots dans la phrase: ')
+console.log('Le 61iem caractere: ')
+console.log('Remplacer \'you\' par \'I\' dans toute la phrase: ')
+console.log('Remplacer \'you\' par \'I\' dans toute la phrase: ')
